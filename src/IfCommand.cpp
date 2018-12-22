7b374a428befcfc7bf93d94b30a8dd7028cbac6d
Dvir <dvirsegev@gmail.com>
2018-12-22 19:41:50 +0200

    # added option cal on double numbers

--- src/IfCommand.cpp
+++ src/IfCommand.cpp
@@ -13,11 +13,16 @@
 void IfCommand::execute(const vector<string> &words) {
     //Parser parser;
     ConditionParser conditionParser(index);
-    //if (conditionParser.execute(words)) {
-    //}
+    if (conditionParser.execute(words)) {
+        index++;
+    }
+    else {
+        while(words[index] != "}") index++;
+        index++ ;
+    }
 }
 /**
- * @param i cons'.
+ * @param index cons'.
  */
-IfCommand::IfCommand(unsigned int &i) : ConditionParser(i){}
+IfCommand::IfCommand(unsigned int &index) : index(index){}
 
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,18 @@
 #include "ExpressionsParser.h"
 #include "list"
 #include "Lexer.h"
+#include "Parser.h"
 
 BindingTable *BindingTable::s_instance = 0;
 SymbolTable *SymbolTable::s_instance = 0;
 PathsTable *PathsTable::s_instance = 0;
 
 int main() {
-//    std::cout << ExpressionsParser::shuntingYardAlg("3-(-4)");
+  std::cout << ExpressionsParser::shuntingYardAlg("3 * 10.2");
     Lexer lexer;
     vector<string> words = lexer.active("file.txt");
+    Parser parser;
+    parser.createFunction(words);
+    //parser.commandParse(words);
     return 0;
 }
