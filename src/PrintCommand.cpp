2aa7c8e86959997527dd769c7eaafad4a58405ff
aviv <avivdavid2312@gmail.com>
2018-12-26 09:06:28 +0200

    update

--- src/PrintCommand.cpp
+++ src/PrintCommand.cpp
@@ -8,12 +8,18 @@ void PrintCommand::execute(const vector<string> &line) {
     // Checks if output is in symbol map. if so, prints his value. else, prints output
     globalMutex.lock();
     index++;
-    output = line[index];
+    while(line[index][line[index].length() - 1] != '"' && !SymbolTable::instance()->atTable(line[index])){
+        output += line[index];
+        output += " ";
+        ++index;
+    }
+    output += line[index];
+    ++index;
     !SymbolTable::instance()->atTable(output) ? cout << output.substr(1, output.length() - 2) :
     cout << SymbolTable::instance()->getValue((output));
+    output = "";
     globalMutex.unlock();
     cout << "\n";
-    index++;
 }
 
 PrintCommand::PrintCommand(unsigned int &index) : index(index) {}
