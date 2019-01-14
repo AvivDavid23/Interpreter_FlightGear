9897ce6c06d07c084b55f64c9bad57b212468903
aviv <avivdavid2312@gmail.com>
2019-01-14 16:11:34 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,13 @@
 #include "A_star.h"
 
 int main(int argc, char **argv) {
-    MatrixMaze matrixMaze(50, 50);
-    string start = "0,0";
-    matrixMaze.setStart(start);
-    string end = "39,39";
-    matrixMaze.setGoal(end);
+    string s1 = "1,2,3\n";
+    string s2 = "5,6,7\n";
+    string s3 = "8,9,10\n";
+    string s4 = "0,0\n";
+    string s5 = "2,2\n";
+    std::vector<string> vector1 = {s1 ,s2 ,s3 ,s4 ,s5};
+    MatrixMaze matrixMaze(vector1);
     cout << matrixMaze.to_string();
-    DFS<Position> dfs;
-    BFS<Position> bfs;
-    A_star<Position> a_star;
-    BestFirstSearch<Position> bestFirstSearch;
-
-    cout << bestFirstSearch.search(&matrixMaze) << endl;
-    cout << to_string(bestFirstSearch.getEvaluatedNodes()) + "," + to_string(matrixMaze.getShortestPathCost()) + "\n";
-    cout << dfs.search(&matrixMaze) << endl;
-    cout << to_string(dfs.getEvaluatedNodes()) + "," + to_string(matrixMaze.getShortestPathCost()) + "\n";
-    cout << bfs.search(&matrixMaze) << endl;
-    cout << to_string(bfs.getEvaluatedNodes()) + "," + to_string(matrixMaze.getShortestPathCost()) + "\n";
-    cout << a_star.search(&matrixMaze) << endl;
-    cout << to_string(a_star.getEvaluatedNodes()) + "," + to_string(matrixMaze.getShortestPathCost()) + "\n";
     return 0;
 }
